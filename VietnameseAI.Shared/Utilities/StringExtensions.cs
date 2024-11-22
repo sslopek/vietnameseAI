39a2084e53a6bb12163e118000f071234000940b
Steven Slopek <slopek@gmail.com>
2024-11-21 20:38:47 -0500

    Change word table to interactive QuickGrid
    Allows the list to be sorted by column and having paging.  I could have done the filtering before, but it was also in the QuickGrid example.
    
    I added StringExtensions to make it possible to search while ignoring Vietnamese diacritics.
    
    Reference: https://aspnet.github.io/quickgridsamples/sample

---
+++ VietnameseAI.Shared/Utilities/StringExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace VietnameseAI.Shared.Utilities;
+public static class StringExtensions
+{
+	public static bool ContainsIgnoringAccentsAndCase(this string source, string target)
+	{
+		ArgumentNullException.ThrowIfNull(source);
+		ArgumentNullException.ThrowIfNull(target);
+
+		string normalizedSource = RemoveDiacritics(source);
+		string normalizedSubstring = RemoveDiacritics(target);
+
+		return normalizedSource.ToLower().Contains(normalizedSubstring.ToLower());
+	}
+
+	private static string RemoveDiacritics(string text)
+	{
+		if (string.IsNullOrEmpty(text))
+		{
+			return text;
+		}
+
+		// Normalize the string to FormD (decomposed)
+		var normalizedString = text.Normalize(NormalizationForm.FormD);
+
+		// Remove diacritics by filtering out non-letter characters
+		var stringBuilder = new StringBuilder();
+		foreach (var c in normalizedString)
+		{
+			var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+			if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+			{
+				stringBuilder.Append(c);
+			}
+		}
+
+		// Normalize back to FormC (composed)
+		return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
+	}
+}
